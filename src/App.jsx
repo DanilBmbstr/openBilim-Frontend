bbfaf298ad8f3cf4eaf1dc67ea779452c9c3107d
name <191105danil@gmail.com>
2025-11-24 13:13:52 +0500

    Авторизация

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,63 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Component from './component/Component' // Правильный импорт
 import Header from './component/Header'
+import Login from './component/Login'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+
+
+  const url = "http://localhost:8080"
+
+  var token = ""
+  const [name, setName] = useState("")
+  const [group, setGroup] = useState("")
+
+
+
+
+  const xhr = new XMLHttpRequest();
+
+  function authorization(log, pas)
+  {
+          token = ""
+          xhr.open('POST', url + '/auth');
+          xhr.setRequestHeader("Content-Type", "application/json");
+          xhr.onload = function() {
+      if (xhr.status === 200) {
+        if(xhr.responseText == "Error: User not found") {setName("Error: User not found"); setGroup("Ошибка: неверный логин или пароль"); return}
+        token = (xhr.responseText);
+
+
+         xhr.open('POST', url + '/getUserData');
+          xhr.setRequestHeader("Content-Type", "application/json");
+          xhr.onload = function() {
+      if (xhr.status === 200) {
+
+        setName(JSON.parse(xhr.responseText).fio);
+        setGroup(JSON.parse(xhr.responseText).group);
+      }}
+          xhr.send(token);
+      }}
+          xhr.send(JSON.stringify({
+             "login": log,
+              "password": pas
+        }
+      
+      
+      ));
+
+      
+  }
+
 
   return (
     <>
 
-          <Header name="Имя Фамилия Отчество" group = "Уч.Группа" discipline = "Дисциплина" testName = "Название теста"/>
-      <div>
+      <Header name={name} group={group} discipline="Дисциплина" testName="Название теста" />
+      <div className="content">
 
-
+        <Login onClick={authorization}></Login>
 
 
 
